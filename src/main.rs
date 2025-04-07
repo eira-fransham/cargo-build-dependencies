19a8b610535b2c61f65f39d8c4553eb2f0521779
Eira Fransham <eira@fourieraudio.com>
2025-04-07 19:00:32 +0200

    Support workspaces, allow excluding packages, allow ignoring errors

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,43 @@ extern crate quickcheck_macros;
 use clap::{App, Arg};
 
 use std::env;
-use std::process::Command;
+use std::process::{Command, ExitStatus, ExitCode};
 
 mod package;
 
-fn main() {
+fn main() -> ExitCode {
     let matched_args = App::new("cargo build-dependencies")
         .arg(Arg::with_name("build-dependencies"))
         .arg(Arg::with_name("release").long("release"))
-        .arg(Arg::with_name("target").long("target"))
+        .arg(Arg::with_name("profile").long("profile").takes_value(true))
+        .arg(Arg::with_name("target").long("target").takes_value(true))
+        .arg(Arg::with_name("ignore-errors").long("ignore-errors"))
+        .arg(
+            Arg::with_name("exclude")
+                .long("exclude")
+                .short("x")
+                .takes_value(true)
+                .multiple(true),
+        )
         .get_matches();
 
     let is_release = matched_args.is_present("release");
+    let profile = match matched_args.value_of("profile") {
+        Some(value) => {
+            if is_release {
+                panic!("`--release` is short for `--profile release`, you should only specify one of these flags");
+            } else {
+                value
+            }
+        }
+        None => {
+            if is_release {
+                "release"
+            } else {
+                "dev"
+            }
+        }
+    };
     let target = match matched_args.value_of("target") {
         Some(value) => value,
         None => "",
@@ -26,7 +51,16 @@ fn main() {
 
     let cargo_toml = package::get_toml("Cargo.toml").expect("Can't get Cargo.toml");
     let cargo_lock = package::get_toml("Cargo.lock").expect("Can't get Cargo.lock");
-    let dependencies = package::get_dependencies(&cargo_toml, &cargo_lock);
+    let mut dependencies = package::get_dependencies(&cargo_toml, &cargo_lock);
+
+    if let Some(excluded_packages) = matched_args.values_of("exclude") {
+        for excluded in excluded_packages {
+            let existing = dependencies.remove(excluded);
+            if existing.is_none() {
+                panic!("{} was marked as excluded, but it is not a dependency of the top-level `Cargo.toml`", excluded);
+            }
+        }
+    }
 
     if dependencies.is_empty() {
         panic!("Can't find dependencies");
@@ -34,24 +68,35 @@ fn main() {
 
     println!("Start building packages");
 
-    for dependency in dependencies {
-        build_package(&dependency, is_release, &target);
+    let mut exit_status = ExitStatus::default();
+
+    for (name, version) in dependencies {
+        let package_status = build_package(
+            &format!("{}:{}", name, version),
+            profile,
+            &target,
+            matched_args.is_present("ignore-errors"),
+        );
+
+        if !package_status.success() {
+            exit_status = package_status;
+        }
     }
 
     println!("Finished");
+
+    if exit_status.success() {
+        ExitCode::SUCCESS
+    } else {
+        ExitCode::FAILURE
+    }
 }
 
-fn build_package(pkg_name: &str, is_release: bool, target: &str) {
+fn build_package(pkg_name: &str, profile: &str, target: &str, ignore_errors: bool) -> ExitStatus {
     println!("Building package: {:?}", pkg_name);
 
     let mut command = Command::new("cargo");
-    let command_with_args = command.arg("build").arg("-p").arg(pkg_name);
-
-    let command_with_args = if is_release {
-        command_with_args.arg("--release")
-    } else {
-        command_with_args
-    };
+    let command_with_args = command.args(["build", "-p", pkg_name, "--profile", profile]);
 
     let command_with_args = if !target.is_empty() {
         command_with_args.arg("--target=".to_string() + target)
@@ -59,10 +104,16 @@ fn build_package(pkg_name: &str, is_release: bool, target: &str) {
         command_with_args
     };
 
-    execute_command(command_with_args);
+    execute_command(command_with_args, |status| {
+        if ignore_errors {
+            eprintln!("{}", status)
+        } else {
+            panic!("{}", status)
+        }
+    })
 }
 
-fn execute_command(command: &mut Command) {
+fn execute_command<F: FnOnce(String)>(command: &mut Command, report: F) -> ExitStatus {
     let mut child = command
         .envs(env::vars())
         .spawn()
@@ -72,8 +123,10 @@ fn execute_command(command: &mut Command) {
 
     if !exit_status.success() {
         match exit_status.code() {
-            Some(code) => panic!("Exited with status code: {}", code),
-            None => panic!("Process terminated by signal"),
+            Some(code) => report(format!("Exited with status code: {}", code)),
+            None => report(format!("Process terminated by signal")),
         }
     }
+
+    exit_status
 }
--- src/package/mod.rs
+++ src/package/mod.rs
@@ -1,7 +1,11 @@
 use toml::Value as Toml;
 
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::prelude::*;
+
+/// Map from package to version.
+type Packages = HashMap<String, String>;
 
 pub fn get_toml(file_path: &str) -> std::io::Result<Toml> {
     let mut toml_file = File::open(file_path)?;
@@ -11,31 +15,35 @@ pub fn get_toml(file_path: &str) -> std::io::Result<Toml> {
     Ok(toml_string.parse().expect("Failed to parse toml"))
 }
 
-pub fn get_dependencies<'a>(cargo_toml: &'a Toml, cargo_lock: &'a Toml) -> Vec<String> {
+pub fn get_dependencies<'a>(cargo_toml: &'a Toml, cargo_lock: &'a Toml) -> Packages {
     let dependencies = get_toml_dependencies(cargo_toml);
     get_lock_dependencies(cargo_lock, &dependencies)
 }
 
-fn get_lock_dependencies<'a>(cargo_lock: &'a Toml, dependencies: &Vec<String>) -> Vec<String> {
+fn get_lock_dependencies<'a>(cargo_lock: &'a Toml, dependencies: &HashSet<String>) -> Packages {
     match cargo_lock.get("package") {
         Some(&Toml::Array(ref packages)) => get_packages(&packages.clone(), dependencies),
-        Some(_) => vec![],
-        None => vec![],
+        Some(_) => Default::default(),
+        None => Default::default(),
     }
 }
 
-fn get_toml_dependencies<'a>(cargo_toml: &'a Toml) -> Vec<String> {
-    match cargo_toml.get("dependencies") {
+fn get_toml_dependencies<'a>(cargo_toml: &'a Toml) -> HashSet<String> {
+    match cargo_toml.get("dependencies").or_else(|| {
+        cargo_toml
+            .get("workspace")
+            .and_then(|workspace| workspace.get("dependencies"))
+    }) {
         Some(&Toml::Table(ref packages)) => packages
             .into_iter()
             .map(|(name, _value)| name.to_string())
             .collect(),
-        Some(_) => vec![],
-        None => vec![],
+        Some(_) => Default::default(),
+        None => Default::default(),
     }
 }
 
-fn get_packages(packages: &Vec<Toml>, dependencies: &Vec<String>) -> Vec<String> {
+fn get_packages(packages: &Vec<Toml>, dependencies: &HashSet<String>) -> Packages {
     packages
         .into_iter()
         .filter_map(|package| match package {
@@ -43,7 +51,7 @@ fn get_packages(packages: &Vec<Toml>, dependencies: &Vec<String>) -> Vec<String>
                 let name = get_string_field(map.get("name"));
                 let version = get_string_field(map.get("version"));
                 if dependencies.contains(&name.to_string()) {
-                    Some(format!("{}:{}", name, version))
+                    Some((name.to_string(), version.to_string()))
                 } else {
                     None
                 }
